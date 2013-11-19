0a14325fcfeaaa384fe0367ea984479d9dcb313d
Michael Atkisson <mpatkisson@gmail.com>
2013-11-19 01:56:27 -0500

    Added a dummy modification.

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.get('/tasks', task.getAll);
 app.post('/task/save', task.save);
 app.post('/task/delete', task.delete);
 app.get('/angular/tasks', task.angular);
+app.get('/angular2/tasks', task.angular);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
