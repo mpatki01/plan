2d530104647d475b7768fbb0e49f9c6bbd00dbb5
Michael Atkisson <mpatkisson@gmail.com>
2013-12-12 00:46:44 -0500

    Moved importation logic into its own module.

--- import/expedia/import-image-data.js
+++ import/expedia/import-image-data.js
@@ -1,91 +1,20 @@
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
 var lineReader = require('line-reader');
+var Importer = require('./importer.js');
 
-var DataImporter = function (callback) {
+
+var importer = new Importer({
+    database: 'triptacular',
+    collection: 'images',
+    filename: './data/HotelImageList.sample',
+    threshold: 10000
+});
+importer.import(function (err) {
     'use strict';
-    var self = this;
-    self.host = 'localhost';
-    self.port = 27017;
-    self.database = 'triptacular';
-    self.collection = 'images';
-    self.filename = './data/HotelImageList.sample';
-    self.threshold = 50000;
-    self.processed = 0;
-    self.inserted = 0;
-    self.linesInFile = 0;
-    self.records = [];
-    self.isLastLine = false;
-    self.callback = callback;
-
-    self.parse = function (line) {
-        var record = null;
-        if (line) {
-            var fields = line.split('|');
-            record = {
-                expediaHotelId: parseInt(fields[0], 10),
-                caption: fields[1],
-                url: fields[2],
-                width: parseInt(fields[3], 10),
-                height: parseInt(fields[4], 10),
-                thumbnailUrl: fields[6],
-                isDefault: fields[7] === '1' ? true : false
-            };
-        }
-        return record;
-    };
-
-    self.processLine = function (line, collection) {
-        var record = self.parse(line);
-        self.records.push(record);
-        var recordable = self.records.length === self.threshold || self.isLastLine;
-        if (recordable) {
-            collection.insert(self.records, { w: 1, fsync: true }, function (err, result) {
-                if (err) {
-                    self.callback(err);
-                }
-
-                self.inserted = self.inserted + result.length;
-                console.log(self.inserted + ' records inserted.');
-                if (self.processed = self.linesInFile &&
-                    self.inserted === self.linesInFile) {
-                    self.callback(null);
-                }
-            });
-            self.records = [];
-        }
-        self.processed = self.processed + 1;
-    };
-
-    self.ingest = function () {
-        var mongoServer = new Server(self.host, self.port);
-        var mongoClient = new MongoClient(mongoServer);
-        mongoClient.open(function (err, mongoClient) {
-            if (err) {
-                self.callback(err);
-            }
-
-            var db = mongoClient.db(self.database);
-            var collection = db.collection(self.collection);
-            var lines = 0;
-            lineReader.eachLine(self.filename, function (line, isLastLine) {
-                lines = lines + 1;
-                if (isLastLine) {
-                    self.isLastLine = isLastLine;
-                    self.linesInFile = lines;
-                }
-                self.processLine(line, collection);
-            });
-        });
-    };
-};
-
-var ingestor = new DataImporter(function (err) {
     if (err) {
         console.log(err);
     }
     console.log('done');
     process.exit(0);
 });
-ingestor.ingest();
-
---
+++ import/expedia/importer.js
@@ -0,0 +1,89 @@
+var MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    lineReader = require('line-reader');
+
+var DataImporter = function (options) {
+    'use strict';
+    var self = this;
+    options = options || {};
+    self.host = options.host || 'localhost';
+    self.port = options.port || 27017;
+    self.database = options.database;
+    self.collection = options.collection;
+    self.filename = options.filename;
+    self.threshold = options.threshold || 100;
+    self.callback = null;
+    self.processed = 0;
+    self.inserted = 0;
+    self.linesInFile = 0;
+    self.records = [];
+    self.isLastLine = false;
+
+    self.parse = function (line) {
+        var record = null;
+        if (line) {
+            var fields = line.split('|');
+            record = {
+                expediaHotelId: parseInt(fields[0], 10),
+                caption: fields[1],
+                url: fields[2],
+                width: parseInt(fields[3], 10),
+                height: parseInt(fields[4], 10),
+                thumbnailUrl: fields[6],
+                isDefault: fields[7] === '1' ? true : false
+            };
+        }
+        return record;
+    };
+
+    self.processLine = function (line, collection) {
+        var record = self.parse(line);
+        self.records.push(record);
+        var recordable = self.records.length === self.threshold || self.isLastLine;
+        if (recordable) {
+            collection.insert(self.records, { w: 1, fsync: true }, function (err, result) {
+                if (err) {
+                    self.callback(err);
+                }
+
+                self.inserted = self.inserted + result.length;
+                console.log(self.inserted + ' records inserted.');
+                if (self.processed === self.linesInFile &&
+                    self.inserted === self.linesInFile) {
+                    self.callback(null);
+                }
+            });
+            self.records = [];
+        }
+        self.processed = self.processed + 1;
+    };
+
+    self.import = function (callback) {
+        if (!callback) {
+            console.log('The import function requires a callback.');
+            return;
+        }
+        self.callback = callback;
+        var mongoServer = new Server(self.host, self.port);
+        var mongoClient = new MongoClient(mongoServer);
+        mongoClient.open(function (err, mongoClient) {
+            if (err) {
+                self.callback(err);
+            }
+
+            var db = mongoClient.db(self.database);
+            var collection = db.collection(self.collection);
+            var lines = 0;
+            lineReader.eachLine(self.filename, function (line, isLastLine) {
+                lines = lines + 1;
+                if (isLastLine) {
+                    self.isLastLine = isLastLine;
+                    self.linesInFile = lines;
+                }
+                self.processLine(line, collection);
+            });
+        });
+    };
+};
+
+module.exports = DataImporter;
