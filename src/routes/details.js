c4c5d16231a5f371563e544ca595790fccd82f2c
Michael Atkisson <mpatkisson@gmail.com>
2017-12-24 22:36:07 -0500

    Adds route for the details model

---
+++ src/routes/details.js
@@ -0,0 +1,22 @@
+/*jslint
+    node
+*/
+
+'use strict';
+
+function init(app, db) {
+
+    // Gets a detail by ID.
+    app.get('/details/detail/:id', function (req, res) {
+        var id = req.params.id;
+        db.details.find({
+            where: {id: id}
+        }).then(function (detail) {
+            res.json(detail);
+        });
+    });
+}
+
+module.exports = {
+    init: init
+};
